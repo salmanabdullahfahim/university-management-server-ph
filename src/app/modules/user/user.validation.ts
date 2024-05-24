b624d8783ba5324f0f86acf683b333de991ec22e
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-05-24 07:58:55 +0600

    refactor user validation and schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,14 +1,11 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
-  id: z.string(),
   password: z
-    .string()
+    .string({
+      invalid_type_error: 'Password must be a string',
+    })
     .max(20, { message: 'Password can not be more than 20 characters' }),
-  needsPasswordChange: z.boolean().optional().default(true),
-  role: z.enum(['admin', 'student', 'faculty']),
-  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-  isDeleted: z.boolean().optional().default(false),
 });
 
 export default userValidationSchema;
