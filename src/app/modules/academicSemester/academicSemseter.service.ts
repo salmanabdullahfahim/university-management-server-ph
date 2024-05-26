80724f7d3ada38bf372d0131f42bb20c3ba472a9
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-05-26 06:43:31 +0600

    added validation to prevent invalid semester code

--- src/app/modules/academicSemester/academicSemseter.service.ts
+++ src/app/modules/academicSemester/academicSemseter.service.ts
@@ -1,7 +1,14 @@
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
 import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  // academic semester name ---> academic semester code
+
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid academic semester code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
