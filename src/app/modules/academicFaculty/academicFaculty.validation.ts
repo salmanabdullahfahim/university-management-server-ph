5a8b783054980193dd2e14f93e8ff1eb8ae711bc
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-05-29 15:54:59 +0600

    academic validation problem fixed

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,13 +1,17 @@
 import { z } from 'zod';
 
 const createAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty name must be a string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty name must be a string',
+    }),
   }),
 });
 const updateAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty name must be a string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty name must be a string',
+    }),
   }),
 });
 
