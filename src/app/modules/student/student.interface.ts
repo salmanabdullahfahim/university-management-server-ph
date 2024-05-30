1fef83fcde4d3b06d74a9885d7363315b1c07b5f
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-05-30 11:49:37 +0600

    add populate in student services to show the reference data

--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -38,5 +38,6 @@ export type Student = {
   localGuardian: LocalGuardian;
   profileImg?: string;
   admissionSemester: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
   isDeleted: boolean;
 };
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,13 +1,27 @@
 import { StudentModel } from './student.model';
 
 const getAllStudentsFromDB = async () => {
-  const result = await StudentModel.find();
+  const result = await StudentModel.find()
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
   return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
   // const result = await StudentModel.findOne({ id: id });
-  const result = await StudentModel.aggregate([{ $match: { id: id } }]);
+  const result = await StudentModel.findById(id)
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
   return result;
 };
 
