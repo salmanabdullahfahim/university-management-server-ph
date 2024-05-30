cde256c3f63c827e23e26e1be82e5e37a31283da
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-05-30 08:17:05 +0600

    validate "is academic department already exists or not"- done

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -19,6 +19,17 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+// check if academic department is already exist
+academicDepartmentSchema.pre('save', async function (next) {
+  const isExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isExist) {
+    throw new Error('Academic department already exist');
+  }
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
