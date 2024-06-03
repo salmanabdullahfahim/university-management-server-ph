c9dba9083fccedcc3fff236c046112e923fd8c99
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-03 11:36:50 +0600

    faculty interface created

---
+++ src/app/modules/Faculty/faculty.interface.ts
@@ -0,0 +1,37 @@
+import { Types } from 'mongoose';
+
+export type TUserName = {
+  firstName: string;
+  lastName: string;
+  middleName?: string;
+};
+
+export type TGender = 'male' | 'female' | 'other';
+
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TFaculty = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TUserName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNumber: string;
+  emergencyContactNumber: string;
+  bloodGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg?: string;
+  academicDepartment: Types.ObjectId;
+  isDeleted: boolean;
+};
