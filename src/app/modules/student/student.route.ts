003ee36a2a301f77fc3d8322023532a00c91370a
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-03 21:36:51 +0600

    refactor student routes for get single student , update & delete

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -5,12 +5,12 @@ import { updateStudentValidationSchema } from './student.zod.validation';
 
 const router = express.Router();
 router.get('/', StudentControllers.getStudents);
-router.get('/:studentId', StudentControllers.getSingleStudent);
+router.get('/:id', StudentControllers.getSingleStudent);
 router.patch(
-  '/:studentId',
+  '/:id',
   validateRequest(updateStudentValidationSchema),
   StudentControllers.updateStudent,
 );
-router.delete('/:studentId', StudentControllers.deleteStudent);
+router.delete('/:id', StudentControllers.deleteStudent);
 
 export const StudentRoutes = router;
