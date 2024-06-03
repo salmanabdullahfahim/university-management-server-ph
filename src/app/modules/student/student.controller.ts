c6cb6088ebcfeb08a8e569cdcf7063c15c5fa964
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-03 21:35:34 +0600

    refactor student controller for get single student and update

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -14,9 +14,9 @@ const getStudents = catchAsync(async (req, res) => {
 });
 
 const getSingleStudent = catchAsync(async (req, res) => {
-  const { studentId } = req.params;
+  const { id } = req.params;
 
-  const result = await StudentServices.getSingleStudentFromDB(studentId);
+  const result = await StudentServices.getSingleStudentFromDB(id);
 
   sendResponse(res, {
     success: true,
@@ -27,9 +27,9 @@ const getSingleStudent = catchAsync(async (req, res) => {
 });
 
 const updateStudent = catchAsync(async (req, res) => {
-  const { studentId } = req.params;
+  const { id } = req.params;
   const { student } = req.body;
-  const result = await StudentServices.updateStudentIntoDB(studentId, student);
+  const result = await StudentServices.updateStudentIntoDB(id, student);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -40,9 +40,9 @@ const updateStudent = catchAsync(async (req, res) => {
 });
 
 const deleteStudent = catchAsync(async (req, res) => {
-  const { studentId } = req.params;
+  const { id } = req.params;
 
-  const result = await StudentServices.deleteStudentFromDB(studentId);
+  const result = await StudentServices.deleteStudentFromDB(id);
 
   sendResponse(res, {
     success: true,
