c26d5fd6ff011ce6644e9cd9a312fce8dcefe62f
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-03 14:51:12 +0600

    admin model created

---
+++ src/app/modules/Admin/admin.constant.ts
@@ -0,0 +1,13 @@
+import { TBloodGroup, TGender } from './admin.interface';
+
+export const Gender: TGender[] = ['male', 'female', 'other'];
+export const BloodGroup: TBloodGroup[] = [
+  'A+',
+  'B+',
+  'AB+',
+  'O+',
+  'A-',
+  'B-',
+  'AB-',
+  'O-',
+];
