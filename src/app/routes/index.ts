dc32b192c3304cbe8dbdf6b27dfdbbab57ec92b2
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-03 11:58:40 +0600

    faculty rote added in the index route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,12 +4,14 @@ import { StudentRoutes } from '../modules/student/student.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
+import { FacultyRoutes } from '../modules/Faculty/faculty.route';
 
 const router = Router();
 
 const moduleRoutes = [
   { path: '/users', route: userRoutes },
   { path: '/students', route: StudentRoutes },
+  { path: '/faculties', route: FacultyRoutes },
   { path: '/academic-semesters', route: AcademicSemesterRoutes },
   { path: '/academic-faculties', route: AcademicFacultyRoutes },
   { path: '/academic-departments', route: AcademicDepartmentRoutes },
