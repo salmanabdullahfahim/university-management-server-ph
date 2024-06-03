8c2da17329c747788cbc598d3a2e5e7b185c57e7
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-03 07:15:15 +0600

    pagination added

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -28,7 +28,7 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
     })),
   });
 
-  const excludeFields = ['searchTerm', 'sort', 'limit'];
+  const excludeFields = ['searchTerm', 'sort', 'limit', 'page'];
   excludeFields.forEach((field) => delete queryObj[field]);
   const filteredResult = searchQuery
     .find(queryObj)
@@ -46,12 +46,21 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   }
   const sortResult = filteredResult.sort(sort);
 
+  let page = 1;
   let limit = 1;
+  let skip = 0;
   if (query?.limit) {
-    limit = query.limit as number;
+    limit = Number(query.limit);
   }
 
-  const limitResult = await sortResult.limit(limit);
+  if (query?.page) {
+    page = Number(query.page);
+    skip = (page - 1) * limit;
+  }
+
+  const paginatedResult = sortResult.skip(skip);
+
+  const limitResult = await paginatedResult.limit(limit);
 
   return limitResult;
 };
