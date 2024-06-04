b7cb2938cf02cc139e5d9a4ef7d00003823c6da9
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-04 07:18:43 +0600

    course model created

---
+++ src/app/modules/Course/course.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { TCourse, TPrerequisiteCourses } from './course.interface';
+
+const prerequisiteCourseSchema = new Schema<TPrerequisiteCourses>({
+  course: {
+    type: Schema.Types.ObjectId,
+    ref: 'Course',
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+const courseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  prefix: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  code: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  prerequisiteCourses: [prerequisiteCourseSchema],
+});
+
+export const Course = model<TCourse>('Course', courseSchema);
