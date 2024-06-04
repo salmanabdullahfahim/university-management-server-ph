2e8c3c76d3ec16e796294a6309aede52771d4fd8
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-04 07:44:08 +0600

    create course validation added with zod

---
+++ src/app/modules/Course/course.validation.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const prerequisiteCoursesValidationSchema = z.object({
+  course: z.string(),
+  isDeleted: z.boolean().optional(),
+});
+
+const createCourseValidationSchema = z.object({
+  body: z.object({
+    title: z.string().max(20),
+    prefix: z.string().max(20),
+    code: z.number(),
+    credits: z.number(),
+    prerequisiteCourses: z.array(prerequisiteCoursesValidationSchema),
+  }),
+});
+
+export const CourseValidations = {
+  createCourseValidationSchema,
+};
