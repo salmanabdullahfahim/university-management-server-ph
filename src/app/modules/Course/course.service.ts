446c91bccdc195cf88c11922fc13ca4148bed545
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-04 14:38:12 +0600

    populate fields in get single course

--- src/app/modules/Course/course.service.ts
+++ src/app/modules/Course/course.service.ts
@@ -23,7 +23,9 @@ const getAllCourseFromDB = async (query: Record<string, unknown>) => {
 };
 
 const getSingleCourseFromDB = async (id: string) => {
-  const result = await Course.findById(id);
+  const result = await Course.findById(id).populate(
+    'prerequisiteCourses.course',
+  );
   return result;
 };
 
