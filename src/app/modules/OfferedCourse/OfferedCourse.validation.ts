01d65c747c0b658bc00baf2d4b63b6d4045943af
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-08 16:38:01 +0600

    fix update offered course validation

--- src/app/modules/OfferedCourse/OfferedCourse.validation.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.validation.ts
@@ -1,6 +1,16 @@
 import { z } from 'zod';
 import { Days } from './OfferedCourse.constant';
 
+const timeStringValidationSchema = z.string().refine(
+  (time) => {
+    const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+    return regex.test(time);
+  },
+  {
+    message: 'Invalid startTime format, expected "HH:MM" in 24 hour format',
+  },
+);
+
 const createOfferedCourseValidationSchema = z.object({
   body: z
     .object({
@@ -12,25 +22,8 @@ const createOfferedCourseValidationSchema = z.object({
       maxCapacity: z.number(),
       section: z.number(),
       days: z.array(z.enum([...Days] as [string, ...string[]])),
-      startTime: z.string().refine(
-        (time) => {
-          const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-          return regex.test(time);
-        },
-        {
-          message:
-            'Invalid startTime format, expected "HH:MM" in 24 hour format',
-        },
-      ),
-      endTime: z.string().refine(
-        (time) => {
-          const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-          return regex.test(time);
-        },
-        {
-          message: 'Invalid endTime format, expected "HH:MM" in 24 hour format',
-        },
-      ),
+      startTime: timeStringValidationSchema,
+      endTime: timeStringValidationSchema,
     })
     .refine(
       (body) => {
@@ -46,13 +39,25 @@ const createOfferedCourseValidationSchema = z.object({
 });
 
 const updateOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    faculty: z.string().optional(),
-    maxCapacity: z.number().optional(),
-    days: z.array(z.enum([...Days] as [string, ...string[]])),
-    startTime: z.string().optional(),
-    endTime: z.string().optional(),
-  }),
+  body: z
+    .object({
+      faculty: z.string(),
+      maxCapacity: z.number(),
+      days: z.array(z.enum([...Days] as [string, ...string[]])),
+      startTime: timeStringValidationSchema,
+      endTime: timeStringValidationSchema,
+    })
+    .refine(
+      (body) => {
+        const start = new Date(`1970-01-01T${body.startTime}`);
+        const end = new Date(`1970-01-01T${body.endTime}`);
+
+        return start < end;
+      },
+      {
+        message: 'Start Time Should be before End Time',
+      },
+    ),
 });
 
 export const OfferedCourseValidations = {
