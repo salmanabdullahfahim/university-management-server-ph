9fac54f75de7ce73af0e57ed5bfa888ae75006df
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-09 08:23:04 +0600

    validate user according to delete blocked and id

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,6 +1,28 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 
-const loginUser = async (payload: TLoginUser) => {};
+const loginUser = async (payload: TLoginUser) => {
+  //  check if the user exists
+  const isUserExists = await User.findOne({ id: payload.id });
+
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
+  // check if the user is deleted
+  const isDeleted = isUserExists?.isDeleted;
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is already deleted');
+  }
+
+  // check if the user is blocked
+  const isBlocked = isUserExists?.status === 'blocked';
+  if (isBlocked) {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is blocked');
+  }
+};
 
 export const AuthServices = {
   loginUser,
