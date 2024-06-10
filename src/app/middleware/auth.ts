ebcbf6fe238867f8d8ac39ccdd2fd7020773ee6f
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-10 07:48:35 +0600

    authorization added with jwt

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import catchAsync from '../utils/catchAsync';
+import AppError from '../errors/AppError';
+import httpStatus from 'http-status';
+
+const auth = () => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You Are Not Unauthorized😈');
+    }
+    next();
+  });
+};
+
+export default auth;
