c0793cef0ab80f1879aae6fa372724440748accc
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-10 08:03:15 +0600

    Verify Token Using Auth

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -2,14 +2,30 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
+    // check if the token is come from client
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You Are Not Unauthorized😈');
     }
-    next();
+
+    // check if the token is valid
+    jwt.verify(token, config.jwt_secret as string, function (err, decoded) {
+      // err
+      if (err) {
+        throw new AppError(
+          httpStatus.UNAUTHORIZED,
+          'You Are Not Unauthorized😈',
+        );
+      }
+      // decoded undefined
+      req.user = decoded as JwtPayload;
+      next();
+    });
   });
 };
 
