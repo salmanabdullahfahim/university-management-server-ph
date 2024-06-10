04647782c11fe554b436a4fb90d935589be0c730
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-10 15:19:37 +0600

    change passsword added

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { AuthController } from './auth.controller';
 import validateRequest from '../../utils/validateRequest';
 import { AuthValidation } from './auth.validation';
+import { USER_ROLE } from '../user/user.constant';
+import auth from '../../middleware/auth';
 
 const router = express.Router();
 
@@ -9,6 +11,13 @@ router.post(
   '/login',
   validateRequest(AuthValidation.loginValidationSchema),
   AuthController.loginUser,
+);
+
+router.post(
+  '/change-password',
+  auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+  validateRequest(AuthValidation.changePasswordValidationSchema),
+  AuthController.changePassword,
 );
 
 export const AuthRoutes = router;
